796f26c1c67b8dc0a58142c2e75eff376f23a61a
psguazz <p.sguazz@gmail.com>
2024-03-07 12:19:15 +0100

    Harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -132,6 +132,27 @@ return {
             require("colorizer").setup()
         end
     },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>o", function() harpoon:list():append() end, { desc = "Add to Harpoon" })
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+                { desc = "Open harpoon window" })
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+            vim.keymap.set("n", "<leader>,", function() harpoon:list():prev() end, { desc = "Previous Harpoon buffer" })
+            vim.keymap.set("n", "<leader>.", function() harpoon:list():next() end, { desc = "Next Harpoon buffer" })
+        end
+    },
     { "gennaro-tedesco/nvim-peekup" },
     { "nvim-treesitter/nvim-treesitter-context" },
     { "mg979/vim-visual-multi" },
