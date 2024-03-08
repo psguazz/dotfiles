8eec5f8e199ce96e224a1abfe16d25423cdf775b
psguazz <p.sguazz@gmail.com>
2024-03-08 10:54:46 +0100

    Tweaking status bar

--- init.lua
+++ init.lua
@@ -220,10 +220,18 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
-        section_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       },
     },
   },
