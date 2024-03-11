9ed3cfc47da8b83458b086967d596b2486b6ecf5
psguazz <p.sguazz@gmail.com>
2024-03-11 22:31:48 +0100

    Path names & previews

--- init.lua
+++ init.lua
@@ -228,7 +228,7 @@ require('lazy').setup({
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -43,7 +43,12 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        window = {
+            mappings = {
+                ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+            }
         },
         config = function()
             vim.keymap.set("n", "<leader>nt", vim.cmd.Neotree)
