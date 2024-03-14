bf990237b23beee43d76d3f22eef00df9836eea1
psguazz <p.sguazz@gmail.com>
2024-03-14 15:02:32 +0100

    Removing selectors
    I don't even know which files are which and the controls conflict with
    others.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -159,11 +159,6 @@ return {
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
                 { desc = "Open harpoon window" })
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
             vim.keymap.set("n", "<leader>,", function() harpoon:list():prev() end, { desc = "Previous Harpoon buffer" })
             vim.keymap.set("n", "<leader>.", function() harpoon:list():next() end, { desc = "Next Harpoon buffer" })
         end
