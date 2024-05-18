74dcf1363e948b344b9a83cb433b2af0a773019d
psguazz <p.sguazz@gmail.com>
2024-05-18 21:44:14 +0200

    Python

--- init.lua
+++ init.lua
@@ -596,7 +596,7 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
