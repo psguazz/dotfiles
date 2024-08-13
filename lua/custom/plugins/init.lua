67fb5f1624162825cd269df38faea152b7eefe01
psguazz <p.sguazz@gmail.com>
2024-08-13 10:25:52 +0200

    TMux stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -141,6 +141,25 @@ return {
             require "alpha".setup(require "alpha.themes.startify".config)
         end
     },
+    {
+        {
+            "christoomey/vim-tmux-navigator",
+            cmd = {
+                "TmuxNavigateLeft",
+                "TmuxNavigateDown",
+                "TmuxNavigateUp",
+                "TmuxNavigateRight",
+                "TmuxNavigatePrevious",
+            },
+            keys = {
+                { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+                { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+                { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+                { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+                { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+            },
+        }
+    },
     { "michaeljsmith/vim-indent-object" },
     { "gennaro-tedesco/nvim-peekup" },
     { "nvim-treesitter/nvim-treesitter-context" },
