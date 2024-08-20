ca629a925150d930798bf83fe99fae1e24b59854
psguazz <p.sguazz@gmail.com>
2024-08-20 12:06:13 +0200

    Bubbly vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -223,15 +223,15 @@ require('lazy').setup({
         icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
-        section_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' }
       },
       sections = {
-        lualine_a = { 'mode' },
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 }, },
       },
     },
   },
