64cd9e9e8cd378caeacb6cfef9e2319eb32896f1
psguazz <p.sguazz@gmail.com>
2024-08-21 20:44:17 +0200

    More session management

--- tmux/plugins/new-session/new-session.sh
+++ tmux/plugins/new-session/new-session.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 START_PATH=$1
-SESSION_NAME="$(basename $START_PATH)"
 
-tmux has-session -t "$SESSION_NAME" 2>/dev/null
+CURRENT_SESSION="$(tmux display-message -p '#S')"
+
+TARGET_SESSION="$(basename $START_PATH)"
+TARGET_SESSION=${TARGET_SESSION//\./}
+
+tmux has-session -t "$TARGET_SESSION" 2>/dev/null
 
 if [ $? != 0 ]; then
-    tmux new-session -d -s $SESSION_NAME -c "$START_PATH"
+    tmux new-session -d -s $TARGET_SESSION -c $START_PATH
+    tmux switch-client -t $TARGET_SESSION
+    tmux switch-client -t $CURRENT_SESSION
+
+    tmux move-window -t $TARGET_SESSION
+    tmux switch-client -t $TARGET_SESSION:2
+    tmux kill-window -a
+else
+    tmux move-window -t $TARGET_SESSION
+    tmux switch-client -t $TARGET_SESSION
 fi
 
-tmux switch-client -t "$SESSION_NAME"
