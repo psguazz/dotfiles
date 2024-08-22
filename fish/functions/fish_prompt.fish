281b15e183742e3152b9fceeeb6c34371670e267
psguazz <p.sguazz@gmail.com>
2024-08-22 21:28:10 +0200

    Actually showing the last command status

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,8 @@
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
+    set -g cmd_status $status
 
     echo -n (set_color '#E7C664')(prompt_pwd)' '
-
-    set_color -o
-    if fish_is_root_user
-        echo -n (set_color red)'# '
-    end
     echo -n (set_color '#FC5D7C')'❯ '
+
     set_color normal
 end
--- fish/functions/item_status.fish
+++ fish/functions/item_status.fish
@@ -1,5 +1,4 @@
 function item_status
-    set -l cmd_status $status
     if test $cmd_status -ne 0
         set_color "#FF6077"
         echo -n "✘ $cmd_status "
