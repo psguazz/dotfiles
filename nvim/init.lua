74d3f963214812255e92c0fe6f38b861493d1e2f
psguazz <p.sguazz@gmail.com>
2024-08-30 18:16:28 +0200

    Removing unused mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -784,14 +784,6 @@ vim.wo.wrap = false
 
 vim.opt.mouse = ""
 
--- File viewer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- Copy/paste to/from clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
 -- Moving selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
