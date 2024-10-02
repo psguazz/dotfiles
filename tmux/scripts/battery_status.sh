ec76d9a1361c8685686b1f7b2459a4b6300e14e0
psguazz <p.sguazz@gmail.com>
2024-10-02 21:59:15 +0200

    Proper battery icon

--- tmux/scripts/battery_status.sh
+++ tmux/scripts/battery_status.sh
@@ -1,23 +1,43 @@
 #!/usr/bin/env bash
 
+function level_icon() {
+    local level=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+
+    if [[ "$level" -ge 90 ]]; then
+        echo "󰁹"
+    elif [[ "$level" -ge 80 ]]; then
+        echo "󰂁"
+    elif [[ "$level" -ge 70 ]]; then
+        echo "󰂀"
+    elif [[ "$level" -ge 60 ]]; then
+        echo "󰁿"
+    elif [[ "$level" -ge 50 ]]; then
+        echo "󰁾"
+    elif [[ "$level" -ge 40 ]]; then
+        echo "󰁽"
+    elif [[ "$level" -ge 30 ]]; then
+        echo "󰁼"
+    elif [[ "$level" -ge 20 ]]; then
+        echo "󰁻"
+    elif [[ "$level" -ge 10 ]]; then
+        echo "󰁺"
+    else
+        echo "󰂎"
+    fi
+}
+
 function battery_status() {
     local status=$(pmset -g batt | sed -n 2p | cut -d ";" -f 2 | tr -d " ")
 
     case $status in
-        discharging|Discharging)
-            echo "󰁿"
-            ;;
-        high|Full)
-            echo "󰁹"
-            ;;
-        charging|Charging)
+        charging|charged)
             echo "󱐌"
             ;;
         acattached|ACattached)
             echo ""
             ;;
         *)
-            echo "󱐌"
+            echo $(level_icon)
             ;;
     esac
 }
