5e36675df0a40c9754c707b4e821be76f7b8d975
psguazz <p.sguazz@gmail.com>
2024-10-24 11:47:40 +0200

    Wording

--- tmux/scripts/switch-session.sh
+++ tmux/scripts/switch-session.sh
@@ -2,7 +2,7 @@
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-session=$(tmux list-sessions -F "#{session_name}" | fzf --prompt "Switch Session ❯ " --exit-0 --reverse --preview "$current_dir/tmux-tree.sh {}" --preview-label " Session Tree ")
+session=$(tmux list-sessions -F "#{session_name}" | fzf --prompt "Switch Session ❯ " --exit-0 --reverse --preview "$current_dir/tmux-tree.sh {}" --preview-label " Sessions ")
 
 tmux switch-client -t=$session 2>/dev/null
 
