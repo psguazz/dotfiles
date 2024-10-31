63ccf17ee6fd0553d2b2f0a62d3573149b43215e
psguazz <p.sguazz@gmail.com>
2024-10-31 14:42:52 +0100

    Showing icons

--- nvim/lua/tabline.lua
+++ nvim/lua/tabline.lua
@@ -1,5 +1,23 @@
 local harpoon = require("harpoon")
 local git = require("lib.git")
+local devicons = require("nvim-web-devicons")
+
+function format(group, text)
+  return "%#" .. group .. "#" .. text
+end
+
+function icon(name, apply_color)
+  local icon, icon_color = devicons.get_icon_color(name, vim.fn.fnamemodify(name, ":e"), { default = true })
+
+  if apply_color then
+    local group_name = "TabLineColor" .. icon_color:gsub("#", "")
+    vim.api.nvim_set_hl(0, group_name, { fg = icon_color })
+
+    return format(group_name, icon)
+  else
+    return icon
+  end
+end
 
 function _G.harpoon_tabline()
   local names = harpoon:list():display()
@@ -10,7 +28,6 @@ function _G.harpoon_tabline()
   for i, name in ipairs(names) do
     local is_current = string.match(current_name, name .. "$")
     local status = git.status(name)
-    local tab_name = vim.fn.fnamemodify(name, ":t")
 
     local group = "Tabline"
     if is_current then group = group .. "Selected" end
@@ -19,14 +36,16 @@ function _G.harpoon_tabline()
     local name_group = group .. "Name"
     if status ~= "None" then name_group = name_group .. status end
 
-    local tab = string.format("%%#%s#  %d%%#%s# %s  ", number_group, i, name_group, tab_name)
+    local number = format(number_group, i)
+    local icon = icon(name, is_current)
+    local tab_name = format(name_group, vim.fn.fnamemodify(name, ":t"))
+
+    local tab = "  " .. number .. " " .. icon .. " " .. tab_name .. "  "
 
     tabline = tabline .. tab
   end
 
-  tabline = tabline .. "%#TablineBackground#"
-
-  return tabline
+  return tabline .. "%#TablineBackground#"
 end
 
 vim.api.nvim_create_autocmd("BufWritePost", {
