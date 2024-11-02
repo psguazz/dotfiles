e83197d38638e2204bd9f77f69ebdb915f7bcc8e
psguazz <p.sguazz@gmail.com>
2024-11-02 20:15:40 +0100

    Removing unused plugins

--- nvim/lua/plugins/cellular-automaton.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "Eandrju/cellular-automaton.nvim",
-    config = function()
-      vim.api.nvim_create_user_command("Rain", "CellularAutomaton make_it_rain", {})
-      vim.api.nvim_create_user_command("GOL", "CellularAutomaton game_of_life", {})
-    end
-  }
-}
