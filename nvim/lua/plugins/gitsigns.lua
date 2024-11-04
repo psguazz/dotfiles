bcc32454f5e434ba14fe9d39c400dbb0808d1078
psguazz <p.sguazz@gmail.com>
2024-11-04 22:23:57 +0100

    Navigating git

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -21,9 +21,9 @@ return {
         vim.keymap.set(mode, l, r, opts)
       end
 
-      map({ "n", "v" }, "]c", function()
+      map({ "n", "v" }, "<leader>g.", function()
         if vim.wo.diff then
-          return "]c"
+          return "<leader>g."
         end
         vim.schedule(function()
           gs.next_hunk()
@@ -31,9 +31,9 @@ return {
         return "<Ignore>"
       end, { expr = true, desc = "Jump to next hunk" })
 
-      map({ "n", "v" }, "[c", function()
+      map({ "n", "v" }, "<leader>g,", function()
         if vim.wo.diff then
-          return "[c"
+          return "<leader>g,"
         end
         vim.schedule(function()
           gs.prev_hunk()
@@ -41,31 +41,12 @@ return {
         return "<Ignore>"
       end, { expr = true, desc = "Jump to previous hunk" })
 
-      map("v", "<leader>hs", function()
-        gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-      end, { desc = "stage git hunk" })
-      map("v", "<leader>hr", function()
-        gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-      end, { desc = "reset git hunk" })
-
-      map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
-      map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
-      map("n", "<leader>hS", gs.stage_buffer, { desc = "git Stage buffer" })
-      map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
-      map("n", "<leader>hR", gs.reset_buffer, { desc = "git Reset buffer" })
-      map("n", "<leader>hp", gs.preview_hunk, { desc = "preview git hunk" })
       map("n", "<leader>hb", function()
         gs.blame_line { full = true }
       end, { desc = "git blame line" })
-      map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
-      map("n", "<leader>hD", function()
-        gs.diffthis "~"
-      end, { desc = "git diff against last commit" })
 
       map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
       map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
-
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "select git hunk" })
     end,
   },
 }
