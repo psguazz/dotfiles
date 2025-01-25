6d31a957c955e0cfdecafa251e52b50bcb25161f
psguazz <p.sguazz@gmail.com>
2025-01-26 00:59:08 +0100

    Removing minimap
    Was cute for a bit but I'm never really looking at it.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -27,14 +27,12 @@ local zen = true
 vim.api.nvim_create_user_command("Zon", function()
   zen = true
   vim.cmd("Toff")
-  vim.cmd("Neominimap off")
   vim.cmd("NvimTreeClose")
 end, {})
 
 vim.api.nvim_create_user_command("Zoff", function()
   zen = false
   vim.cmd("Ton")
-  vim.cmd("Neominimap on")
   vim.cmd("NvimTreeOpen")
   vim.cmd("wincmd l")
 end, {})
