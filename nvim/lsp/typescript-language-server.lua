3bd68e8194046697c8bb0841944090ecc1e2679b
psguazz <p.sguazz@gmail.com>
2025-04-10 11:50:54 +0200

    Removing tslsp format options
    Prettier wins anyway.

--- nvim/lsp/typescript-language-server.lua
+++ nvim/lsp/typescript-language-server.lua
@@ -1,28 +1,3 @@
-local format = {
-  indentSize = 4,
-  tabSize = 4,
-  newLineCharacter = "\n",
-  convertTabsToSpaces = true,
-  indentStyle = 2,
-  insertSpaceAfterConstructor = false,
-  insertSpaceAfterCommaDelimiter = true,
-  insertSpaceAfterSemicolonInForStatements = true,
-  insertSpaceBeforeAndAfterBinaryOperators = true,
-  insertSpaceAfterKeywordsInControlFlowStatements = true,
-  insertSpaceAfterFunctionKeywordForAnonymousFunctions = false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
-  insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
-  insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
-  insertSpaceBeforeFunctionParenthesis = false,
-  placeOpenBraceOnNewLineForFunctions = false,
-  placeOpenBraceOnNewLineForControlBlocks = false,
-  semicolons = "insert",
-  trimTrailingWhitespace = true,
-  indentSwitchCase = true,
-}
-
 return {
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = {
@@ -39,8 +14,4 @@ return {
     "package.json",
     ".git"
   },
-  settings = {
-    typescript = { format = format },
-    javascript = { format = format }
-  }
 }
