078bc4c2e831118ec878b9b05d4b723f84b9432e
psguazz <p.sguazz@gmail.com>
2025-04-10 11:39:52 +0200

    Cleaning up init

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,23 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("config.opts")
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  { import = "plugins" }
-}, {})
+require("config.lazy")
 
 require("config.diagnostics")
 require("config.lsp")
