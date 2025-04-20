079e9f7562f0b35f23b18986f73c058c4ae86882
psguazz <p.sguazz@gmail.com>
2025-04-20 02:24:38 +0200

    Page navigation in visual mode

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -3,8 +3,8 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>j", "<c-d>zz")
-vim.keymap.set("n", "<leader>k", "<c-u>zz")
+vim.keymap.set({ "n", "v" }, "<leader>j", "<c-d>zz")
+vim.keymap.set({ "n", "v" }, "<leader>k", "<c-u>zz")
 
 vim.keymap.set("n", "<leader>\"", ":split<CR>")
 vim.keymap.set("n", "<leader>%", ":vsplit<CR>")
