edb5f06e379abca8fa5521d4d324ce0900d04058
psguazz <p.sguazz@gmail.com>
2025-04-28 17:35:32 +0200

    Cleaning up

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 sketchybar --add item front_app center \
-           --set front_app icon.drawing=off \
-           script="$PLUGIN_DIR/front_app.sh" \
+           --set front_app script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
-
-
--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  sketchybar --set "$NAME" label="$INFO" icon="$(/Users/psg/.config/scripts/icon_map_fn.sh "$INFO")" icon.font="sketchybar-app-font:Regular:16.0"
 fi
