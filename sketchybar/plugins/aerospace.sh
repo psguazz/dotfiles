f835808e90a4ef567bf5e10a0ec7383b020da5ee
psguazz <p.sguazz@gmail.com>
2025-04-29 01:48:36 +0200

    Updating workspace icons

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -12,7 +12,20 @@ else
   label=$grey
 fi
 
-sketchybar --set $NAME \
+icon_strip=""
+apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+
+if [ "${apps}" != "" ]; then
+  while read -r app; do
+    icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
+  done <<<"${apps}"
+
+  sketchybar --set $NAME \
            background.color="$(hex_to_rgba $bg)" \
            icon.color=$(hex_to_rgba $icon) \
-           label.color=$(hex_to_rgba $label)
+           label="$icon_strip" \
+           label.color=$(hex_to_rgba $label) \
+           drawing=on
+else
+  sketchybar --set $NAME drawing=off
+fi
