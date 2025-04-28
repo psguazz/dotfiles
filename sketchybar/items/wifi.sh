90d8f532c555a99c77854fb677b32f414464be59
psguazz <p.sguazz@gmail.com>
2025-04-28 22:33:05 +0200

    Removing all the things

--- sketchybar/items/wifi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item wifi right \
-           --set wifi script="$PLUGIN_DIR/wifi.sh"
-           --subscribe wifi wifi_change
--- sketchybar/plugins/battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SCRIPTS_DIR="/Users/psg/.config/scripts"
-
-ICON=$($SCRIPTS_DIR/battery_status.sh)
-
-sketchybar --set "$NAME" icon="$ICON" icon.font="Monaco Nerd Font:Bold:20.0"
