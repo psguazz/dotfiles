97d25c6aead00acd68ea1f4e6badf830b88e3a30
psguazz <p.sguazz@gmail.com>
2025-04-29 11:31:07 +0200

    Sender doesn't work
    Why why why

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,34 +2,30 @@
 
 source "/Users/psg/.config/scripts/palette.sh"
 
-if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    bg=$diff_yellow
-    icon=$grey
-    label=$fg
-  else
-    bg=$bg_dim
-    icon=$grey
-    label=$grey
-  fi
-
-  sketchybar --set $NAME \
-             background.color="$(hex_to_rgba $bg)" \
-             icon.color=$(hex_to_rgba $icon) \
-             label.color=$(hex_to_rgba $label)
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  bg=$diff_yellow
+  icon=$grey
+  label=$fg
+else
+  bg=$bg_dim
+  icon=$grey
+  label=$grey
 fi
 
-if [ "$SENDER" = "space_windows_change" ]; then
-  icon_strip=""
-  apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+sketchybar --set $NAME \
+           background.color="$(hex_to_rgba $bg)" \
+           icon.color=$(hex_to_rgba $icon) \
+           label.color=$(hex_to_rgba $label)
 
-  if [ "${apps}" != "" ]; then
-    while read -r app; do
-      icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
-    done <<<"${apps}"
+icon_strip=""
+apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
-    sketchybar --set $NAME label="$icon_strip" drawing=on
-  else
-    sketchybar --set $NAME drawing=off
-  fi
+if [ "${apps}" != "" ]; then
+  while read -r app; do
+    icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
+  done <<<"${apps}"
+
+  sketchybar --set $NAME label="$icon_strip" drawing=on
+else
+  sketchybar --set $NAME drawing=off
 fi
