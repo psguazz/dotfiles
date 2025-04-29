c951b10769f601d255721d0bddbad8e340520d2f
psguazz <p.sguazz@gmail.com>
2025-04-29 15:02:51 +0200

    Filtering events

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,28 +2,32 @@
 
 source "/Users/psg/.config/scripts/palette.sh"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  color=$yellow
-  border=1
-else
-  color=$grey
-  border=0
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    color=$yellow
+    border=1
+  else
+    color=$grey
+    border=0
+  fi
+
+  sketchybar --set $NAME \
+             background.border_width="$border" \
+             icon.color=$(hex_to_rgba $grey) \
+             label.color=$(hex_to_rgba $color)
 fi
 
-sketchybar --set $NAME \
-           background.border_width="$border" \
-           icon.color=$(hex_to_rgba $grey) \
-           label.color=$(hex_to_rgba $color)
+if [ "$SENDER" = "space_windows_change" ]; then
+  icon_strip=""
+  apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
-icon_strip=""
-apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+  if [ "${apps}" != "" ]; then
+    while read -r app; do
+      icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
+    done <<<"${apps}"
 
-if [ "${apps}" != "" ]; then
-  while read -r app; do
-    icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
-  done <<<"${apps}"
-
-  sketchybar --set $NAME label="$icon_strip" drawing=on
-else
-  sketchybar --set $NAME drawing=off
+    sketchybar --set $NAME label="$icon_strip" drawing=on
+  else
+    sketchybar --set $NAME drawing=off
+  fi
 fi
