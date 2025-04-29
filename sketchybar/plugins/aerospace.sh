2401a4ad6c456b38ef0d16cedec13415ea37ead2
psguazz <p.sguazz@gmail.com>
2025-04-29 23:08:24 +0200

    Frequent aerospace update
    Removing the event filter and updating everything all the time is not
    particularly efficient, but gives a quick way to fix nonsense sync
    issues.

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,32 +2,28 @@
 
 source "/Users/psg/.config/scripts/palette.sh"
 
-if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    color=$yellow
-    border=1
-  else
-    color=$grey
-    border=0
-  fi
-
-  sketchybar --set $NAME \
-             background.border_width="$border" \
-             icon.color=$(hex_to_rgba $grey) \
-             label.color=$(hex_to_rgba $color)
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  color=$yellow
+  border=1
+else
+  color=$grey
+  border=0
 fi
 
-if [ "$SENDER" = "space_windows_change" ]; then
-  icon_strip=""
-  apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+sketchybar --set $NAME \
+  background.border_width="$border" \
+  icon.color=$(hex_to_rgba $grey) \
+  label.color=$(hex_to_rgba $color)
 
-  if [ "${apps}" != "" ]; then
-    while read -r app; do
-      icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
-    done <<<"${apps}"
+icon_strip=""
+apps=$(aerospace list-windows --workspace "$1" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
-    sketchybar --set $NAME label="$icon_strip" drawing=on
-  else
-    sketchybar --set $NAME drawing=off
-  fi
+if [ "${apps}" != "" ]; then
+  while read -r app; do
+    icon_strip+=" $(/Users/psg/.config/scripts/icon_map_fn.sh "$app")"
+  done <<<"${apps}"
+
+  sketchybar --set $NAME label="$icon_strip" drawing=on
+else
+  sketchybar --set $NAME drawing=off
 fi
