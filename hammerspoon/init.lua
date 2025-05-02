2a38144e53c5b8a9eca3b35dc24b75bbc189e3b0
psguazz <p.sguazz@gmail.com>
2025-05-02 11:51:15 +0200

    Cleaning up code
    And addressing the flicker when pressing `option` a bunch of times

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,29 +1,38 @@
 local sketchybar_path = "/usr/local/bin/sketchybar"
 
-local hideDelay = 0.7
-local hideTimer = nil
+local hide_delay = 0.7
+local hide_timer = nil
 
-local function setSketchybarTopmost(state)
+local function set_bar(state)
   hs.task.new(sketchybar_path, nil, { "--bar", "topmost=" .. state }):start()
 end
 
-local optionWatcher = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(ev)
+local function show_bar()
+  if hide_timer then
+    hide_timer:stop()
+    hide_timer = nil
+  else
+    set_bar("on")
+  end
+end
+
+local function hide_bar()
+  if not hide_timer then
+    hide_timer = hs.timer.doAfter(hide_delay, function()
+      set_bar("off")
+      hide_timer = nil
+    end)
+  end
+end
+
+local option_watcher = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(ev)
   local flags = ev:getFlags()
 
   if flags.alt then
-    if hideTimer then
-      hideTimer:stop()
-      hideTimer = nil
-    end
-    setSketchybarTopmost("on")
+    show_bar()
   else
-    if not hideTimer then
-      hideTimer = hs.timer.doAfter(hideDelay, function()
-        setSketchybarTopmost("off")
-        hideTimer = nil
-      end)
-    end
+    hide_bar()
   end
 end)
 
-optionWatcher:start()
+option_watcher:start()
