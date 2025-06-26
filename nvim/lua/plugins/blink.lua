4ca2c6a4268196891e0f3c85ff1db6d453e711cc
psguazz <p.sguazz@gmail.com>
2025-06-26 16:19:01 +0200

    Default autocomplete keys

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,12 +3,6 @@ return {
   dependencies = { "rafamadriz/friendly-snippets" },
   version = "1.*",
   opts = {
-    keymap = {
-      preset = "none",
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<Tab>"] = { "accept", "fallback" },
-    },
     appearance = {
       nerd_font_variant = "normal"
     },
