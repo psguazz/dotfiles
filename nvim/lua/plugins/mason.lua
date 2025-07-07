475a77d0e4d27e29340de4a461d1c868d69f5eff
Paolo <psg@Mac.home>
2025-07-07 19:51:59 +0200

    Better feedback from Mason
    Was failing silently before :(

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
     require("mason").setup()
 
     local mason_registry = require("mason-registry")
+
     local servers = {
       "elixir-ls",
       "erb-formatter",
@@ -16,13 +17,21 @@ return {
       "typescript-language-server",
     }
 
-    for _, server_name in ipairs(servers) do
-      local ok, pkg = pcall(mason_registry.get_package, server_name)
-      if not ok then
-        vim.notify("LSP not found: " .. server_name, vim.log.levels.ERROR)
-      elseif not pkg:is_installed() then
-        pkg:install()
+    mason_registry.refresh(function()
+      for _, server_name in ipairs(servers) do
+        local ok, pkg = pcall(mason_registry.get_package, server_name)
+        if not ok then
+          vim.notify("LSP not found: " .. server_name, vim.log.levels.ERROR)
+        elseif not pkg:is_installed() then
+          pkg:on("install:success", function()
+            vim.notify("LSP installed: " .. server_name)
+          end)
+          pkg:on("install:failed", function()
+            vim.notify("Failed to install LSP: " .. server_name, vim.log.levels.ERROR)
+          end)
+          pkg:install()
+        end
       end
-    end
+    end)
   end
 }
