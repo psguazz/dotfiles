e02d5940359027caa16f4d4fcfdce92759e1c6c2
psguazz <p.sguazz@gmail.com>
2025-11-24 15:37:02 +0100

    Fixing opencode

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -1,14 +1,13 @@
 local function ask(cmd)
   vim.cmd("silent! write")
-  require("opencode").ask(cmd)
+  require("opencode").ask(cmd, { submit = true })
   vim.cmd("checktime")
 end
 
 return {
   "NickvanDyke/opencode.nvim",
   keys = {
-    { "<leader>op", function() ask() end,               desc = "Ask opencode", },
-    { "<leader>oa", function() ask("@cursor: ") end,    desc = "Ask opencode about this",      mode = "n", },
-    { "<leader>oa", function() ask("@selection: ") end, desc = "Ask opencode about selection", mode = "v", },
+    { "<leader>op", function() ask() end,          desc = "Ask opencode", },
+    { "<leader>oa", function() ask("@this: ") end, desc = "Ask opencode about this" },
   },
 }
