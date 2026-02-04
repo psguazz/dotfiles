e7410167bcf56855161241dda0ea4bdfa97e4d6e
psguazz <p.sguazz@gmail.com>
2026-02-04 18:25:57 +0100

    Poor man's 99 maybe?

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -4,11 +4,25 @@ local function ask(cmd)
   vim.cmd("checktime")
 end
 
+local function prompt(cmd)
+  vim.cmd("silent! write")
+  require("opencode").prompt(cmd, { submit = true })
+  vim.cmd("checktime")
+end
+
+TODO_PROMPT = [[
+  @this: Read the TODO comment and replace it with the ncessary code.
+  Feel free to look around the code base and infer more details on what's supposed to be done.
+  When you have a plan, replace the TODO comment with your implementation.
+  Do NOT touch anything else: all the changes must be localized to the function or block where the comment is.
+  If you think the task requires more changes in different places, tell me, but do NOT implement them yet.
+]]
+
 return {
   "NickvanDyke/opencode.nvim",
   keys = {
-    { "<leader>op", function() ask() end,          desc = "Ask opencode", },
-    { "<leader>oa", function() ask("@this: ") end, desc = "Ask opencode about this" },
+    { "<leader>oa", function() ask("@this: ") end,      desc = "Ask opencode about this" },
+    { "<leader>of", function() prompt(TODO_PROMPT) end, desc = "Address a TODO comment" },
   },
   config = function()
     vim.g.opencode_opts = {
