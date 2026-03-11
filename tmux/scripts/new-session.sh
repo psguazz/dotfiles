3045972cb4833a687afbb154583bf89c2edc3867
psguazz <p.sguazz@gmail.com>
2026-03-11 23:09:14 +0100

    Handling spaces

--- tmux/scripts/new-session.sh
+++ tmux/scripts/new-session.sh
@@ -2,18 +2,18 @@
 
 start_path=$(find ~ ~/paradem ~/projects ~/master_ai -mindepth 1 -maxdepth 1 -type d | fzf --prompt "New Session ❯ " --exit-0 --reverse)
 
-target_session="$(basename $start_path)"
+target_session="$(basename "$start_path")"
 target_session=${target_session//\./}
 
 tmux has-session -t="$target_session" 2>/dev/null
 
 if [ $? != 0 ]; then
-    tmux new-session -d -s $target_session -c $start_path
-    tmux new-window -t $target_session -c $start_path
-    tmux new-window -t $target_session -c $start_path
-    tmux select-window -t $target_session:2
-    tmux send-keys -t $target_session:2 "nvim" C-m
-    tmux send-keys -t $target_session:2 ":Zoff" C-m
+    tmux new-session -d -s "$target_session" -c "$start_path"
+    tmux new-window -t "$target_session" -c "$start_path"
+    tmux new-window -t "$target_session" -c "$start_path"
+    tmux select-window -t "$target_session":2
+    tmux send-keys -t "$target_session":2 "nvim" C-m
+    tmux send-keys -t "$target_session":2 ":Zoff" C-m
 fi
 
-tmux switch-client -t=$target_session
+tmux switch-client -t="$target_session"
--- tmux/scripts/switch-session.sh
+++ tmux/scripts/switch-session.sh
@@ -4,5 +4,5 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 session=$(tmux list-sessions -F "#{session_name}" | fzf --prompt "Switch Session ❯ " --exit-0 --reverse --preview "$current_dir/tmux-tree.sh {} {q}" --preview-label " Sessions ")
 
-tmux switch-client -t=$session 2>/dev/null
+tmux switch-client -t="$session" 2>/dev/null
 
