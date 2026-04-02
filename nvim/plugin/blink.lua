6b6a2570e96ff659412e02cba18db2ebe3bf71d1
psguazz <p.sguazz@gmail.com>
2026-04-03 00:57:38 +0200

    Some magic for blink

--- nvim/plugin/blink.lua
+++ nvim/plugin/blink.lua
@@ -1,7 +1,20 @@
-vim.pack.add({
-  src = "saghen/blink.cmp",
-  version = "1.*",
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "blink.cmp" then
+      local res = vim.system({ "cargo", "build", "--release" }, { cwd = ev.data.path })
+      if vim.v.shell_error ~= 0 then
+        vim.notify("Failed to compile blink.cmp: " .. res, vim.log.levels.ERROR)
+      else
+        vim.notify("Successfully compiled blink.cmp", vim.log.levels.INFO)
+      end
+    end
+  end,
 })
+
+vim.pack.add({{
+  src = "https://github.com/saghen/blink.cmp",
+  version = "v1.10.1",
+}})
 
 require("blink.cmp").setup({
   appearance = {
