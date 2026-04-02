75f0b674e705c026fae66503ba0060dcc312a71c
psguazz <p.sguazz@gmail.com>
2026-04-03 01:37:43 +0200

    Moving mods and theme to after

---
+++ nvim/after/plugin/init.lua
@@ -0,0 +1,2 @@
+require("config.theme")
+require("config.mods")
--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,4 @@ require("config.opts")
 require("config.diagnostics")
 require("config.lsp")
 require("config.mappings")
-require("config.theme")
 require("config.text-objects")
-
-require("config.mods")
