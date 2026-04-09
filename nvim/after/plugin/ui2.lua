1b7c0177ff473bd3eb5372df38864b665ce372db
psguazz <p.sguazz@gmail.com>
2026-04-09 11:48:27 +0200

    Enabling ui2

---
+++ nvim/after/plugin/ui2.lua
@@ -0,0 +1,19 @@
+require("vim._core.ui2").enable({
+  enable = true,
+  msg = {
+    targets = "msg",
+    cmd = {
+      height = 0.5
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.5,
+      timeout = 4000,
+    },
+    pager = {
+      height = 1,
+    },
+  },
+})
