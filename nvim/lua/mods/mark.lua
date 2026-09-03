b93e25c8857ca62a94f8bc996c8513ebc22c6d03
psguazz <p.sguazz@gmail.com>
2026-09-04 00:53:18 +0200

    WORKING with multicursor

--- nvim/lua/mods/mark.lua
+++ nvim/lua/mods/mark.lua
@@ -1,13 +1,5 @@
 local current_pattern = nil
 
-local function escape(text)
-  return vim.fn.escape(text, "/\\")
-end
-
-local function feed(keys)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "x", false)
-end
-
 local function input(keys)
   vim.api.nvim_input(keys)
 end
@@ -18,36 +10,29 @@ local function move(count, dir)
 end
 
 local function current_selection()
-  local start = vim.fn.getpos("'<")
-  local finish = vim.fn.getpos("'>")
+  local start = vim.fn.getpos("v")
+  local finish = vim.fn.getpos(".")
 
   if start[2] ~= finish[2] then return end
 
-  vim.cmd('noau normal! "vy"')
-  return escape(vim.fn.getreg("v"))
+  local line = start[2] - 1
+  local scol = start[3] - 1
+  local ecol = finish[3]
+  local text = vim.api.nvim_buf_get_text(0, line, scol, line, ecol, {})[1]
+
+  return vim.fn.escape(text, "/\\")
 end
 
-local function new_selection()
-  vim.cmd("normal! viw")
-  return current_selection()
-end
-
-local function add_cursor(get_selection)
-  if current_pattern == nil then current_pattern = get_selection() end
+local function add_cursor()
+  if current_pattern == nil then current_pattern = current_selection() end
   if current_pattern == nil then return end
 
   local n = #current_pattern - 1
-  local mode = vim.api.nvim_get_mode().mode
 
-  if mode ~= "n" then feed("<Esc>") end
-  feed("2q=")
-
-  vim.fn.setreg("/", "\\V" .. current_pattern)
-  move(n, "l")
-
+  input("<Esc>")
   input("Q")
-  input("n")
-  input(" l")
+  input("/\\V" .. current_pattern .. "<CR>")
+  input(":noh<CR>")
 
   input("2q=")
   move(n, "l")
@@ -66,8 +51,8 @@ end
 local M = {}
 
 function M.setup()
-  vim.keymap.set("n", "<C-n>", function() add_cursor(new_selection) end, { noremap = true, silent = true })
-  vim.keymap.set("v", "<C-n>", function() add_cursor(current_selection) end, { noremap = true, silent = true })
+  vim.keymap.set("n", "<C-n>", function() input("viw") end, { noremap = true, silent = true })
+  vim.keymap.set("v", "<C-n>", function() add_cursor() end, { noremap = true, silent = true })
 
   vim.keymap.set("n", "<Esc>", clear_cursors)
 end
